3d0e9d6794baefec7ae3472a6391d8a97c3936e9
yulei <yulei@addcn.com>
2020-06-29 23:56:31 +0800

    feat: add five lesson

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -11,4 +11,19 @@ export interface AxiosRequestConfig {
   method?: Method
   data?: any
   params?: any
+  headers?: any
+  responseType?: XMLHttpRequestResponseType
+}
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {
+
 }
